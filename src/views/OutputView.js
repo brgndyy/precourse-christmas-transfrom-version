188034dedf8891f206817519a104c58b6f3fc517
brgndy <brgndy96@gmail.com>
2023-12-14 13:24:10 +0900

    Feat: OutputView에서 시작 메세지 출력 함수 작성

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,11 +1,19 @@
 import { Console } from '@woowacourse/mission-utils';
+import DELIMITER from '../constants/delimiters/delimiter.js';
+import PROGRESS_MESSAGE from '../constants/messages/progressMessage.js';
 
 const OutputView = {
-  printMenu() {
-    Console.print('<주문 메뉴>');
-    // ...
+  printStartMessage() {
+    Console.print(PROGRESS_MESSAGE.start);
   },
-  // ...
+
+  printErrorMessage(message) {
+    Console.print(message);
+  },
+
+  divideLine() {
+    Console.print(DELIMITER.space);
+  },
 };
 
 export default OutputView;

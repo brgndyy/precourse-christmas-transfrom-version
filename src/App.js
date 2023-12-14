a23353b6c687db50be3db7c8c36afb3098a5fbe4
brgndy <brgndy96@gmail.com>
2023-12-14 13:49:36 +0900

    Feat: README.md에 있는 3번 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import OutputView from './views/OutputView.js';
 import asyncFunctionHandlerWithError from './utils/asyncFunctionHandlerWithError.js';
 import InputView from './views/InputView.js';
 import VisitDateValidator from './validators/VisitDateValidator.js';
+import OrderableMoneyValidator from './validators/OrderableMoneyValidator.js';
 
 class App {
   #visitDate;
 
+  #orderableMoney;
+
   async run() {
     this.#printStartMessage();
     await asyncFunctionHandlerWithError(this.#readVisitDate, this);
+    await asyncFunctionHandlerWithError(this.#readOrderableMoney, this);
   }
 
   #printStartMessage() {
@@ -19,6 +23,16 @@ class App {
   async #readVisitDate() {
     this.#visitDate = await InputView.readVisitDate();
     VisitDateValidator.validateVisitDate(this.#visitDate);
+
+    OutputView.divideLine();
+  }
+
+  async #readOrderableMoney() {
+    this.#orderableMoney = await InputView.readOrderableMoney();
+    OrderableMoneyValidator.validateOrderableMoney(this.#orderableMoney);
+    OutputView.divideLine();
+    OutputView.printOrderableMoney(this.#orderableMoney);
+    OutputView.divideLine();
   }
 }
 

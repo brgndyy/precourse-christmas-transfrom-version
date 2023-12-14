03d7db2d80e3ac8ff4d625afa0fdf701efecc343
brgndy <brgndy96@gmail.com>
2023-12-14 16:25:00 +0900

    Feat: 최종적인 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import InputView from './views/InputView.js';
 import VisitDateValidator from './validators/VisitDateValidator.js';
 import OrderableMoneyValidator from './validators/OrderableMoneyValidator.js';
 import OrderMachine from './domain/OrderMachine.js';
+import DiscountMachine from './domain/DiscountMachine.js';
+import PROGRESS_MESSAGE from './constants/messages/progressMessage.js';
+import EventBadgeChecker from './domain/EventBadgeChecker.js';
 
 class App {
   #visitDate;
@@ -12,12 +15,21 @@ class App {
 
   #totalOrderedList;
 
+  #discountListExceptFreeGift;
+
+  #totalPriceBeforeDiscount;
+
+  #totalDiscountPrice;
+
   async run() {
     this.#printStartMessage();
     await asyncFunctionHandlerWithError(this.#readVisitDate, this);
     await asyncFunctionHandlerWithError(this.#readOrderableMoney, this);
     this.#printStartOfResultMessage();
     this.#printOrderMenuList();
+    this.#printTotalDiscountList();
+    this.#printExpectedPaymentPrice();
+    this.#printEventBadge();
   }
 
   #printStartMessage() {
@@ -50,9 +62,60 @@ class App {
     const orderedList = orderMachine.generateRandomOrderList();
 
     this.#totalOrderedList = orderedList.orderedList;
+    this.#totalPriceBeforeDiscount = this.#totalOrderedList.reduce(
+      (acc, cur) => acc + cur.price,
+      0,
+    );
 
+    OutputView.printTotalPriceBeforeDiscount(this.#totalPriceBeforeDiscount);
+    OutputView.divideLine();
     OutputView.printTotalOrderedList(this.#totalOrderedList);
     OutputView.divideLine();
+  }
+
+  #printTotalDiscountList() {
+    const discountMachine = new DiscountMachine(this.#visitDate, this.#totalOrderedList);
+    const discountList = discountMachine.calculateTotalDiscount();
+    const filteredDiscountList = discountList.filter((list) => list.discountPrice > 0);
+    this.#totalDiscountPrice = filteredDiscountList.reduce(
+      (acc, cur) => acc + cur.discountPrice,
+      0,
+    );
+    this.#discountListExceptFreeGift = filteredDiscountList.filter(
+      (list) => list.event !== PROGRESS_MESSAGE.free_gift_event,
+    );
+
+    this.#printDiscountDetails(filteredDiscountList);
+  }
+
+  #printDiscountDetails(filteredDiscountList) {
+    OutputView.printTotalDisountList(filteredDiscountList);
+    OutputView.divideLine();
+    OutputView.printTotalDiscountPrice(this.#totalDiscountPrice);
+    OutputView.divideLine();
+  }
+
+  #printExpectedPaymentPrice() {
+    const totalExpectedPaymentPrcie = this.#discountListExceptFreeGift.reduce(
+      (acc, cur) => acc + cur.discountPrice,
+      0,
+    );
+    const totalPrice = this.#totalPriceBeforeDiscount - totalExpectedPaymentPrcie;
+
+    OutputView.printTotalExpectedPaymentPrice(totalPrice);
+    OutputView.divideLine();
+    this.#printAllChange(this.#totalPriceBeforeDiscount - totalPrice);
+  }
+
+  #printAllChange(change) {
+    OutputView.printChange(change);
+    OutputView.divideLine();
+  }
+
+  #printEventBadge() {
+    const eventBadge = EventBadgeChecker.getEventBadge(this.#totalDiscountPrice);
+
+    OutputView.printEventBadge(eventBadge);
   }
 }
 
--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -18,10 +18,46 @@ const OutputView = {
     Console.print(PROGRESS_MESSAGE.result_start(date));
   },
 
+  printTotalPriceBeforeDiscount(price) {
+    Console.print(PROGRESS_MESSAGE.total_price_before_discount_banner);
+    Console.print(PROGRESS_MESSAGE.total_price_before_discount(formatMoney(price)));
+  },
+
   printTotalOrderedList(orderedList) {
+    Console.print(PROGRESS_MESSAGE.order_list_banner);
     orderedList.forEach((menu) => {
       Console.print(PROGRESS_MESSAGE.ordered_menu(menu.menu));
     });
+  },
+
+  printTotalDisountList(filteredDiscountList) {
+    Console.print(PROGRESS_MESSAGE.discount_list_banner);
+    filteredDiscountList.forEach((list) => {
+      const formattedPrice = formatMoney(list.discountPrice);
+      Console.print(PROGRESS_MESSAGE.discount_list(list.event, formattedPrice));
+    });
+  },
+
+  printTotalDiscountPrice(totalDiscountPrice) {
+    Console.print(PROGRESS_MESSAGE.discount_price_banner);
+    Console.print(PROGRESS_MESSAGE.total_discount_price(formatMoney(totalDiscountPrice)));
+  },
+
+  printTotalExpectedPaymentPrice(price) {
+    Console.print(PROGRESS_MESSAGE.total_expected_payment_price_banner);
+
+    Console.print(PROGRESS_MESSAGE.total_expected_payment_price(formatMoney(price)));
+  },
+
+  printChange(change) {
+    Console.print(PROGRESS_MESSAGE.change_banner);
+
+    Console.print(PROGRESS_MESSAGE.change(formatMoney(change)));
+  },
+
+  printEventBadge(eventBadge) {
+    Console.print(PROGRESS_MESSAGE.event_badge_banner);
+    Console.print(eventBadge);
   },
 
   printErrorMessage(message) {

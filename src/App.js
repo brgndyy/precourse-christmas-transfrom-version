a19a6495e5e00bdabfe4e3ce7387bd847ce87857
brgndy <brgndy96@gmail.com>
2023-12-14 13:34:34 +0900

    Feat: 방문날짜 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import OutputView from './views/OutputView.js';
+import asyncFunctionHandlerWithError from './utils/asyncFunctionHandlerWithError.js';
+import InputView from './views/InputView.js';
+import VisitDateValidator from './validators/VisitDateValidator.js';
 
 class App {
   #visitDate;
 
   async run() {
     this.#printStartMessage();
+    await asyncFunctionHandlerWithError(this.#readVisitDate, this);
   }
 
   #printStartMessage() {
     OutputView.printStartMessage();
     OutputView.divideLine();
+  }
+
+  async #readVisitDate() {
+    this.#visitDate = await InputView.readVisitDate();
+    VisitDateValidator.validateVisitDate(this.#visitDate);
   }
 }
 
--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,11 +1,11 @@
 import { Console } from '@woowacourse/mission-utils';
+import PROGRESS_MESSAGE from '../constants/messages/progressMessage.js';
 
 const InputView = {
-  async readDate() {
-    const input = await Console.readLineAsync(
-      '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
-    );
-    // ...
+  async readVisitDate() {
+    const input = await Console.readLineAsync(PROGRESS_MESSAGE.read_visit_date);
+
+    return Number(input);
   },
   // ...
 };
